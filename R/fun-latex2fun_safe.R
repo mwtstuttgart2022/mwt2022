3388f2d4279c3b64f2788ab5a5a077e65af3b69b
Ioan Scheffel <mwt.stuttgart.2022@gmail.com>
2022-05-10 12:27:26 +0200

    Added tests for latex2fun_safe

--- R/fun-latex2fun_safe.R
+++ R/fun-latex2fun_safe.R
@@ -1,21 +1,35 @@
 #' @title latex2fun_safe
-#'
-#' @param code : character string in Latex language
-#'
-#' @return Latex expression translated into R code
+#' @description Works similar to latex2r::latex2fun with additional input constraints
+#' @param latex_string : character string in Latex language
+#' @return fun_out : function object built from Latex string input
 #' @include fun-latex2r_safe.R
 #' @import dplyr
-latex2fun_safe = function (latex_string)
-{
+latex2fun_safe = function (latex_string) {
+
+  #translate latex string
+
   fun_body = latex2r_safe(latex_string)
-  variables <- all.vars(
-    as.formula(paste("~", fun_body))
-  )
-  if (grepl("=", fun_body)) {
-    stop("Expression contains assignment.")
-  }
-  if (variables[!variables %in% c("x", "pi")] %>% length() != 0) {
-    stop("Expression contains other variable than x.")
-  }
-  pryr::make_function(alist(x = ), parse(text = fun_body)[[1]])
+
+  #error catching
+
+  ##contains assignment
+
+  stopifnot("Expression contains assignment." = !grepl("=", fun_body))
+
+  ##contains illegal variables
+
+  ###extract variables from function body
+
+  variables <- fun_body %>% paste("~", .) %>% as.formula() %>% all.vars()
+
+  stopifnot(
+    "Expression contains other variable than x." =
+      variables[!variables %in% c("x", "pi")] %>% length() == 0
+    )
+
+  #write new function
+
+  fun_out <- pryr::make_function(alist(x = ), parse(text = fun_body)[[1]])
+
+  return(fun_out)
 }
---
+++ tests/testthat/test-fun-latex2fun_safe.R
@@ -0,0 +1,63 @@
+#' @import greekLetters
+
+test_that("latex2fun_safe works", {
+  #existence
+
+  testthat::expect_true(exists("latex2fun_safe", mode = "function"))
+
+  #catch error
+
+  ##empty input
+
+  testthat::expect_error(latex2fun_safe())
+
+  ##contains assignment
+
+  testthat::expect_error(latex2fun_safe("x = 7"))
+
+  ##contains illegal variables
+
+  ###create illegal latex expressions
+
+  library(greekLetters)
+  test_string_vec <-
+    c(
+      letters[!letters %in% c("x", "e")],
+      names(greek) %>% .[.!= "pi"] %>% paste0("\\", .)
+    ) %>%
+    sapply(paste0, "\\cdot x") %>%
+    unname()
+
+  ###initialize output
+
+  actual_out_vec <- c()
+
+  for (latex_string in test_string_vec) {
+
+
+    actual_out <- tryCatch(latex2fun_safe(latex_string), error = function(cond) "err")
+
+    if (actual_out %>% is.function()) {
+      actual_out_vec <-
+        c(
+          actual_out_vec,
+          TRUE
+        )
+    }
+      else
+        actual_out_vec <-
+          c(
+            actual_out_vec,
+            FALSE
+          )
+
+    }
+
+
+  testthat::expect_equal(actual_out_vec, rep(FALSE, length(test_string_vec)))
+
+
+
+
+
+})
